6f9bce0643c2052101cdda8781e20ec5e3cd5a2e
Saurabh Kumar <42462285+saurabhkumarsaurabhkumar@users.noreply.github.com>
2022-02-15 16:34:22 +0530

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,14 +1,24 @@
+import imp
+from operator import imod
+from random import random
 from tkinter import *
 from tkinter import ttk,messagebox
-from PIL import Image,ImageTk #pip install pillow
+from PIL import Image,ImageTk
+from click import command #pip install pillow
 import pymysql #pip install pymysql
+
+import string
+import random
+
+import smtplib
+
 class Register:
     def __init__(self,root):
         self.root=root
         self.root.title("Home")
         self.root.geometry("1350x700+0+0")
         # self.root.config(bg="green")
-        title=Label(self.root,text="Student Management System",bd=10,relief=GROOVE,font=("times new roman",40,"bold"),bg="grey",fg="yellow")
+        title=Label(self.root,text="Employee Management System",bd=10,relief=GROOVE,font=("times new roman",40,"bold"),bg="grey",fg="yellow")
         title.pack(side=TOP,fill=X)
 
 
@@ -27,7 +37,7 @@ class Register:
         manage_frame=Frame(self.root,bd=4,relief=RIDGE,bg="crimson")
         manage_frame.place(x=20,y=100,width=450,height=590)
 
-        m_title=Label(manage_frame,text="Manage Students",bg="crimson",fg="white",font=("times new roman",30,"bold"))
+        m_title=Label(manage_frame,text="Manage Employee",bg="crimson",fg="white",font=("times new roman",30,"bold"))
         m_title.grid(row=0,columnspan=2,pady=20)
 
         lbl_roll=Label(manage_frame,text="Roll No",bg="crimson",fg="white",font=("times new roman",20,"bold"))
@@ -70,12 +80,14 @@ class Register:
 
      #######--Button Frame--#########
         btn_frame=Frame(manage_frame,bd=4,relief=RIDGE,bg="crimson")
-        btn_frame.place(x=10,y=500,width=430)
+        btn_frame.place(x=0,y=500,width=460)
 
-        Addbtn=Button(btn_frame,text="Add",width=10,command=self.add_student).grid(row=0,column=0,padx=10,pady=10)
-        Updatebtn=Button(btn_frame,text="Update",width=10,command=self.update_data).grid(row=0,column=1,padx=10,pady=10)
-        deletebtn=Button(btn_frame,text="Delete",width=10,command=self.delete_data).grid(row=0,column=2,padx=10,pady=10)
-        clearbtn=Button(btn_frame,text="Clear",command=self.clear,width=10).grid(row=0,column=3,padx=10,pady=10)
+        Addbtn=Button(btn_frame,text="Add",width=10,command=self.add_employee).grid(row=0,column=0,padx=5,pady=10)
+        Updatebtn=Button(btn_frame,text="Update",width=10,command=self.update_data).grid(row=0,column=1,padx=5,pady=10)
+        deletebtn=Button(btn_frame,text="Delete",width=10,command=self.delete_data).grid(row=0,column=2,padx=5,pady=10)
+        clearbtn=Button(btn_frame,text="Clear",command=self.clear,width=10).grid(row=0,column=3,padx=5,pady=10)
+        mailsend=Button(btn_frame,text="Mail",command=self.sendEmpMail,width=10).grid(row=0,column=4,padx=5,pady=10)
+
 
     
 
@@ -135,18 +147,44 @@ class Register:
 
 
 
-    def add_student(self):
-        con=pymysql.connect(host="localhost",user="root",password="",database="stm")
-        cur=con.cursor()
-        cur.execute("insert into students values(%s,%s,%s,%s,%s,%s,%s)",(self.Roll_No_var.get(),self.name_var.get(),
-                                                                            self.email_var.get(),self.gender_var.get(),self.contact_var.get(),self.dob_var.get(),
-                                                                            self.txt_address.get('1.0',END)
-        ))
+    def add_employee(self):
+
+        if self.Roll_No_var==" " or self.name_var==" " or self.email_var==" " or self.gender_var==" " or self.contact_var==" " or self.dob_var==" " or self.txt_address==" ":
+            messagebox.showerror("Error","All Fields are required")
+
+        elif self.Roll_No_var!=" " and self.name_var!=" " and self.email_var!=" " and self.gender_var!=" " and self.contact_var!=" " and self.dob_var!=" " and self.txt_address!=" ":
+
+            con=pymysql.connect(host="localhost",user="root",password="",database="stm")
+            cur=con.cursor()
+            cur.execute("insert into students values(%s,%s,%s,%s,%s,%s,%s)",(self.Roll_No_var.get(),self.name_var.get(),
+                                                                                self.email_var.get(),self.gender_var.get(),self.contact_var.get(),self.dob_var.get(),
+                                                                                self.txt_address.get('1.0',END)
+            ))
+
+        print(''.join([random.choice(string.ascii_letters+string.digits) for i in range(6)])) ## Generate a random unique Id upto 6 digit
 
         con.commit()
         self.fetch_data()
         self.clear()
         con.close()
+    def sendEmpMail(self):
+        address_info=self.email_var.get()
+        subject="Credentials Details "+self.name_var.get()
+        email_body="Congratulation :)\nYou details given below\n"+"Emp id :"+self.Roll_No_var.get()+"\n"+"Emp Name :"+self.name_var.get()+"\n"+"Emp Mail :"+self.email_var.get()+"\n"+"Emp Gender :"+self.gender_var.get()+"\n"+"Emp Mobile No :"+self.contact_var.get()+"\n"+"Emp DOB :"+self.dob_var.get()+"\n"+"Emp Address :"+self.txt_address.get("1.0",'end-1c')+"\n"+"Thanks & Regards :\n"+"Saurabh Kumar"+"\n"+"Software Developer at Agatsa Software\n"+"Contact No :"+"7355794240"
+        sub_and_body_msg = 'Subject: {}\n\n{}'.format(subject, email_body)
+        sender_email="nitiansk@gmail.com"
+        send_pass="7237084256"
+
+
+        server=smtplib.SMTP('smtp.gmail.com',587)
+        server.starttls()
+        server.login(sender_email,send_pass)
+
+        print("Login Sucessfully")
+        server.sendmail(sender_email,address_info,sub_and_body_msg)
+        messagebox.showinfo("Success","Congratulation,Details have been sent Sucessfully on your registered mail i'd")
+        print("Messeges have been sent")
+
 
 
     def fetch_data(self):
@@ -187,7 +225,7 @@ class Register:
     def update_data(self):
         con=pymysql.connect(host="localhost",user="root",password="",database="stm")
         cur=con.cursor()
-        cur.execute("update students set name=%s,email=%s,gender=%s,contact=%s,dob=%s,address=%s where roll_no=%s",(self.name_var.get(),
+        cur.execute("update students set name_var=%s,email_var=%s,gender_var=%s,contact_var=%s,dob_var=%s,txt_address=%s where Roll_No_var=%s",(self.name_var.get(),
                                                                             self.email_var.get(),self.gender_var.get(),self.contact_var.get(),self.dob_var.get(),
                                                                             self.txt_address.get('1.0',END),
                                                                             self.Roll_No_var.get()
@@ -202,7 +240,7 @@ class Register:
     def delete_data(self):
         con=pymysql.connect(host="localhost",user="root",password="",database="stm")
         cur=con.cursor()
-        cur.execute("delete from students where roll_no=%s",self.Roll_No_var.get())
+        cur.execute("delete from students where Roll_No_var=%s",self.Roll_No_var.get())
         con.commit()
         con.close()
         self.fetch_data()
@@ -213,7 +251,7 @@ class Register:
     def search_data(self):
         con=pymysql.connect(host="localhost",user="root",password="",database="stm")
         cur=con.cursor()
-        cur.execute("select * from students where "+str(self.search_by.get())+" LIKE '%"+str(self.search_txt.get())+"'%")
+        cur.execute("select * from students where"+"str(self.search_by.get())"+ "LIKE '%'+str(self.search_txt.get()) + '%'")
         rows=cur.fetchall()
         if len(rows)!=0:
             self.Student_table.delete(*self.Student_table.get_children())
